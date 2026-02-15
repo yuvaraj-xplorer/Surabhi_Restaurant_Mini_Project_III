a683994ef71cb5afd5af3132600a8081211fd35b
Yuvaraj <yuvaraj_xplorer@zohomail.in>
2026-02-15 17:44:24 +0530

    DB Details change

--- Data.sql
+++ Data.sql
@@ -9,6 +9,6 @@ INSERT INTO admin(mobile,password,user_email,user_name)
 VALUES("98763456","1234","adminhcl@hcl.com","admin");
 
 INSERT INTO foodlist (cuisine_name, description,name,price, type)VALUES("Semolina","Rice cake","Idly",20.6,"breakfast");
-INSERT INTO foodlist (cuisine_name, description,name,price, type)VALUES("Biriyani","Veg and nonveg","Biriyani",200.00,"ThalappakattiBiriyani");
+INSERT INTO foodlist (cuisine_name, description,name,price, type)VALUES("Biriyani","Veg","Biriyani",200.00,"Thalappakatti");
 INSERT INTO foodlist (cuisine_name, description,name,price, type)VALUES("Semolina","Dosa","RavaDosa",50.00,"breakfast");
 INSERT INTO foodlist (cuisine_name, description,name,price, type)VALUES("Chicken","Pepper","PepperChicken",150.00,"starter");
